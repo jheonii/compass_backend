eb799a98235eecfd55286d1e2f9b71883c5a1cf4
jheonii <jheonii@snu.ac.kr>
2023-03-21 22:00:05 +0900

    test2

--- test.py
+++ test.py
@@ -4,8 +4,9 @@ import pandas as pd
 def analyze():
 
     GSEC = pd.read_csv('GSEC.csv')
+    result_data = GSEC.head(1000)
     print(GSEC.head(10))
-
+    result_json = result_data.transpose().to_json()
     return {
-        "data": 'test'
+        "data": result_json
     }
